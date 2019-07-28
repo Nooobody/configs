5fe5390d291d5b0609e232bf4673d81dbb30d92d
Nooobody <thesilentcoder@outlook.com>
2019-07-28 03:19:55 +0300

    Remove redundancies from tmux, add an init script

---
+++ init.sh
@@ -0,0 +1,18 @@
+
+apt-get update && apt-get upgrade
+apt-get install vim git build-essential curl fish tmux fzf silversearcher-ag
+
+chsh -s /usr/bin/fish
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash  
+
+rm ~/.vimrc
+ln -s ./.vimrc ~/.vimrc
+echo ".vimrc installed!"
+
+rm ~/.tmux.conf
+ln -s ./.tmux.conf ~/.tmux.conf
+echo ".tmux.conf installed!"
+
+rm -rf ~/.config/fish
+ln -s ./fish ~/.config/fish
+echo "Fish installed!"
