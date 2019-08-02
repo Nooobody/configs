87c095c228c0460578b5e9afe49deb43ceb5c89a
Nooobody <thesilentcoder@outlook.com>
2019-08-03 00:14:10 +0300

    Update fish config

--- .bash_profile
+++
@@ -1,9 +0,0 @@
-
-export PATH="$PATH:$HOME/.local/bin"
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-export ANDROID_HOME="/Users/jpu2532/Library/Android/sdk"
-
-export LC_ALL=en_US.UTF-8
--- fish/config.fish
+++ fish/config.fish
@@ -8,4 +8,9 @@ if [ (uname) -eq Darwin ]
   nvm use default --silent
 end
 
-export PATH="$HOME/Library/Android/sdk/tools:$HOME/Library/Android/sdk/platform-tools:$PATH"
+[ -s "$NVM_DIR/nvm.sh" ]; and \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ]; and \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+set -gx PATH ~/.local/bin $PATH
+set -x LC_ALL en_US.UTF-8
+set -x DOCKER_HOST tcp://localhost:2375
